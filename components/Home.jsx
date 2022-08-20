c8b2b6c1c4968bbd5a188aa9ec3c5dcf95173811
choozin <ctaylor17@outlook.com>
2022-08-20 01:50:10 -0400

    Corrected spelling mistake

--- components/Home.jsx
+++ components/Home.jsx
@@ -271,7 +271,7 @@ const Home = () => {
                             color: '#E0DFDF',
                             fontFamily: 'Special Elite',
                         }}>
-                        <p>We are conveniently located in beautiful downtown Forest at the intersection of Highways 21 &amp; Townsend Line on the south-eastern corner.</p>
+                        <p>We are conveniently located in beautiful downtown Forest at the intersection of Highway 21 &amp; Townsend Line on the south-eastern corner.</p>
                         <h4 style={{ marginBottom: '0.5rem', marginTop: '3rem', fontSize: '1.2rem', color: '#FFF', textShadow: '2px 2px black, -2px 2px black, 2px -2px black, -2px -2px black, ' }}>Hours</h4>
                         <p>Monday to Friday<br /><span style={{ fontSize: '1rem' }}>8am to 4pm</span></p><p>Saturday &amp; Sunday<br /><span style={{ fontSize: '1rem' }}>Closed</span></p>
                     </div>
